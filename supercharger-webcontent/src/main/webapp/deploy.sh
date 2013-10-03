a62e71c09afb96348bcc7d62da26f504695282a1
keith <keith@8f12e165-3efa-431a-93b1-af36e02b0b72>
2013-10-03 15:33:41 +0000

    deploy scripts

---
+++ supercharger-webcontent/src/main/webapp/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR_SRC=/tmp/supercharger-webcontent-1.0.?-SNAPSHOT
+DIR_DEPLOY_=/var/www/tesla_wiki/supercharger
+
+rm -rf ${DIR_DEPLOY}/*
+mv ${DIR_SRC}/* ${DIR_DEPLOY}
+chown -R www-data:www-data ${DIR_DEPLOY}
+
+
+rm -r ${DIR_SRC}
