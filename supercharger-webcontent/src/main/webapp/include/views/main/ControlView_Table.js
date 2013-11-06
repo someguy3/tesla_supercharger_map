c83ebbde2a1fd3fdada2df4cf751b5f6536b9f40
keith <keith@8f12e165-3efa-431a-93b1-af36e02b0b72>
2013-11-06 03:11:16 +0000

    implement: right click to add

--- supercharger-webcontent/src/main/webapp/include/views/main/ControlView_Table.js
+++ supercharger-webcontent/src/main/webapp/include/views/main/ControlView_Table.js
@@ -6,34 +6,19 @@ redshiftsoft = createMyNamespace("redshiftsoft");
  */
 redshiftsoft.ControlView_Table = function () {
 
-    this.superChargerMarkTable = $("#supercharger-mark-table");
     this.superChargerDataTable = $("#supercharger-data-table");
 
     this.superData = new redshiftsoft.SuperchargerData();
 
     this.draw();
-
-    this.superChargerMarkTable.click(jQuery.proxy(this.handleTableClick, this));
 }
 
 redshiftsoft.ControlView_Table.prototype.draw = function () {
 
-    var tableBodyMark = this.superChargerMarkTable.find("tbody");
     var tableBodyData = this.superChargerDataTable.find("tbody");
 
     for (var i = 0; i < this.superData.size(); i++) {
         var supercharger = this.superData.get(i);
-
-        tableBodyMark.append("" +
-            "<tr>" +
-            "<td>" + supercharger.displayName + "</td>" +
-            "<td>" + supercharger.address.state + "</td>" +
-            "<td>" + supercharger.address.country + "</td>" +
-            "<td>" + "<a href='" + supercharger.url + "'>link</a></td>" +
-            "<td class='tog'>" + "on" + "</td>" +
-            "<td class='tog'>" + "off" + "</td>" +
-            "</tr>"
-        );
 
         tableBodyData.append("" +
             "<tr>" +
@@ -43,7 +28,7 @@ redshiftsoft.ControlView_Table.prototype.draw = function () {
             "<td>" + supercharger.address.state + "</td>" +
             "<td>" + supercharger.address.zip + "</td>" +
             "<td>" + supercharger.address.country + "</td>" +
-            "<td>" + "<a href='" + supercharger.url + "'>link</a></td>" +
+            "<td class='tog'>" + "<a href='" + supercharger.url + "'>link</a></td>" +
             "</tr>"
         );
 
