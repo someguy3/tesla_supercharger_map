cbc6c1bec6c10a4201d87f608dd0cc67687ec5fe
keith <keith@8f12e165-3efa-431a-93b1-af36e02b0b72>
2013-11-09 00:45:17 +0000

    spelling

--- supercharger-webcontent/src/main/webapp/include/views/main/SuperchargerData.js
+++ supercharger-webcontent/src/main/webapp/include/views/main/SuperchargerData.js
@@ -83,7 +83,7 @@ redshiftsoft.SuperchargerData.prototype.getRegionCount = function (region) {
  *
  * id     -- [Integer] uniquely identifies each record.
  * circle -- [google.maps.Circle] a reference to the google-maps Circle object indicating range for this supercharger.
- * marker -- [google.maps.Marker] a reference to the google-maps Marker object assocated with this supercharger.
+ * marker -- [google.maps.Marker] a reference to the google-maps Marker object associated with this supercharger.
  * custom -- [Boolean] true indicates that this is a custom marker added by the customer.
  *
  */
