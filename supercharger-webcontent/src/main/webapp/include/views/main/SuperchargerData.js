a804f242e6d542cafdb927562918158c4e482d26
keith <keith@8f12e165-3efa-431a-93b1-af36e02b0b72>
2013-12-07 00:08:38 +0000

    Zevenaar now open

--- supercharger-webcontent/src/main/webapp/include/views/main/SuperchargerData.js
+++ supercharger-webcontent/src/main/webapp/include/views/main/SuperchargerData.js
@@ -580,7 +580,6 @@ redshiftsoft.SuperchargerData.LIST = [
         "address": new redshiftsoft.Address("Doesburgseweg 39 6902 PL", "Zevenaar", "", "", "Netherlands"),
         "location": new google.maps.LatLng(51.94066, 6.08180),
         "url": "http://www.teslamotorsclub.com/showthread.php/24215-Supercharger-in-Oost",
-        "construction": true,
         "count": true
     },
     //-----------------------------------------------------------------------------------------------------------------| NORWAY
