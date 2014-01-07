759eb10bd775892035e2e54d8d09c565b08175a9
keith <keith@8f12e165-3efa-431a-93b1-af36e02b0b72>
2014-01-07 23:50:37 +0000

    script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 WEB_APP='supercharger-webcontent-1.?.?-SNAPSHOT'
+REMOTE_HOST=dev.redshiftsoft.com
+SSH_USER=keith
 
-scp -r supercharger-webcontent/target/${WEB_APP} keith@dev:/tmp/
+scp -r supercharger-webcontent/target/${WEB_APP} ${SSH_USER}@${REMOTE_HOST}:/tmp/
 
-ssh keith@dev chmod u+x /tmp/${WEB_APP}/deploy.sh
-ssh keith@dev /tmp/${WEB_APP}/deploy.sh
+ssh ${SSH_USER}@${REMOTE_HOST} chmod u+x /tmp/${WEB_APP}/deploy.sh
+ssh ${SSH_USER}@${REMOTE_HOST} /tmp/${WEB_APP}/deploy.sh
