11288a6cf024db86b470d8afd95a7defedd38db9
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-11 18:07:04 -0600

    requirejs

--- supercharger-webcontent/project/scripts/main.js
+++ supercharger-webcontent/project/scripts/main.js
@@ -1,6 +1,19 @@
-require(["helper/util"], function(util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+requirejs.config({
+    //By default load any module IDs from scrips/lib
+    baseUrl: 'scripts/lib',
+    //except, if the module ID starts with "app",
+    //load it from the scripts/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app'
+    }
 });
+
+// Start the main app logic.
+requirejs(['jquery', 'jquery-ui', 'canvas', 'app/sub'],
+    function ($, canvas, sub) {
+        //jQuery, canvas and the app/sub module are all
+        //loaded and can be used here now.
+    });
