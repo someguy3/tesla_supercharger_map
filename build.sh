d14da6a9e390c30983f928dbdcef9a5f3703d652
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-12 10:34:22 -0600

    requirejs

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cd supercharger-webcontent
-
-#nodejs r.js -o baseUrl=project/scripts paths.jquery=lib/jquery name=main out=main-built.js
-
-
-nodejs r.js -o build.css.js
---
+++ supercharger-webcontent/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+nodejs r.js -o build.css.js
