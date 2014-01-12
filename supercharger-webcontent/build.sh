d8350c35a2fe130759ccc341282b23c005e0fbef
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-12 14:35:06 -0600

    build script improve

--- supercharger-webcontent/build.sh
+++ supercharger-webcontent/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+PROJECT_VERSION=2.0.0
+BUILD_TIMESTAMP=`date +'%b %d %I:%M %Z'`
 DIR_PROJECT='project'
 DIR_BUILT='project-built'
+
+echo "------------------------------------------------"
+echo "PROJECT_VERSION: ${PROJECT_VERSION}"
+echo "BUILD_TIMESTAMP: ${BUILD_TIMESTAMP}"
+echo "DIR_PROJECT    : ${DIR_PROJECT}"
+echo "DIR_BUILT      : ${DIR_BUILT}"
+echo "------------------------------------------------"
+
 
 # CSS
 nodejs r.js -o cssIn=${DIR_PROJECT}/css/main.css out=${DIR_BUILT}/css/main.css
@@ -12,5 +22,10 @@ nodejs r.js -o build.js
 
 cp -R ${DIR_PROJECT}/fonts ${DIR_BUILT}
 cp -R ${DIR_PROJECT}/images ${DIR_BUILT}
-cp -R ${DIR_PROJECT}/index.html ${DIR_BUILT}
+
+cp ${DIR_PROJECT}/* ${DIR_BUILT}
 cp ${DIR_PROJECT}/scripts/require.js ${DIR_BUILT}/scripts
+
+
+sed -i "s/\${build.timestamp}/${BUILD_TIMESTAMP}/g" ${DIR_BUILT}/version.json
+sed -i "s/\${project.version}/${PROJECT_VERSION}/g" ${DIR_BUILT}/version.json
