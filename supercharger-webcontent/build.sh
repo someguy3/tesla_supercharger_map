18d16849643f96d7b92be22798e662bea0b3d8e7
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-12 15:06:34 -0600

    improve

--- supercharger-webcontent/build.sh
+++ supercharger-webcontent/build.sh
@@ -26,6 +26,14 @@ cp -R ${DIR_PROJECT}/images ${DIR_BUILT}
 cp ${DIR_PROJECT}/* ${DIR_BUILT}
 cp ${DIR_PROJECT}/scripts/require.js ${DIR_BUILT}/scripts
 
-
+#
+# Version
+#
 sed -i "s/\${build.timestamp}/${BUILD_TIMESTAMP}/g" ${DIR_BUILT}/version.json
 sed -i "s/\${project.version}/${PROJECT_VERSION}/g" ${DIR_BUILT}/version.json
+
+
+#
+# change log
+#
+# TODO: append --> git log --grep=live --grep='ADD SUPER' --pretty=format:'%cd||%s' | sed 's/ +0000//'
---
+++ supercharger-webcontent/project/scripts/nav/NavBarToAboutPage.js
@@ -0,0 +1,26 @@
+define(['jquery'], function ($) {
+    /**
+     *
+     * @constructor
+     */
+    var NavBarToAboutPage = function () {
+    }
+
+    NavBarToAboutPage.prototype.populateAboutPage = function () {
+        var pageAbout = $("#page-about");
+        if (!pageAbout.data('about-tab-initialized')) {
+            jQuery.getJSON("version.json", function (data) {
+                pageAbout.append("" +
+                    "<br/>" +
+                    "<b>Last Updated: </b>" + data.buildTimestamp + "<br/>" +
+                    "<br/>" +
+                    "Send updates/corrections to <b>map" + "@tes" + "lawiki.net</b>" +
+                    "<br/>"
+                );
+                pageAbout.data('about-tab-initialized', true);
+            });
+        }
+    };
+
+    return NavBarToAboutPage;
+});
