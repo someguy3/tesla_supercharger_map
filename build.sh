d661d39aabe76032d21da17b5a8cb50849ad082a
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-12 10:33:19 -0600

    requirejs

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cd supercharger-webcontent
 
 #nodejs r.js -o baseUrl=project/scripts paths.jquery=lib/jquery name=main out=main-built.js
 
-nodejs r.js -o build.js
+
+nodejs r.js -o build.css.js
---
+++ supercharger-webcontent/build.css.js
@@ -0,0 +1,14 @@
+({
+    appDir: "project",
+    baseUrl: "scripts",
+    dir: "project-built",
+    paths: {
+        jquery: 'empty:',
+        jqueryui: 'empty:'
+    },
+    modules: [
+        {
+            name: "main"
+        }
+    ]
+})
