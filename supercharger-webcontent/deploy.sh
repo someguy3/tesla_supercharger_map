96f36b0d1ce7405cb01cfb7b2615f14a90c76cce
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-12 20:47:06 -0600

    deploy script

--- supercharger-webcontent/deploy.sh
+++ supercharger-webcontent/deploy.sh
@@ -3,7 +3,7 @@
 # Deploy
 #==============================================================================================
 
-WEB_APP='supercharger-webcontent-1.?.?-SNAPSHOT'
+WEB_APP_DIR='project'
 REMOTE_HOST=dev.redshiftsoft.com
 SSH_USER=keith
 DIR_DEPLOY=/var/www/wiki_tesla/supercharger2
@@ -15,6 +15,6 @@ then
     ssh ${SSH_USER}@${REMOTE_HOST} mkdir ${DIR_DEPLOY}
 fi
 
-scp -r supercharger-webcontent/target/${WEB_APP}/* ${SSH_USER}@${REMOTE_HOST}:${DIR_DEPLOY}/
+scp -r ${WEB_APP_DIR}/* ${SSH_USER}@${REMOTE_HOST}:${DIR_DEPLOY}/
 
 ssh ${SSH_USER}@${REMOTE_HOST} chown -R keith:www-data ${DIR_DEPLOY}
