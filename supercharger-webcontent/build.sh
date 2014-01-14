12918f3d0080d08c9e9fb5fa2ffc4b6e1e9393cf
Keith David Winkler <keithdwinkler@gmail.com>
2014-01-13 21:27:11 -0600

    build improvement

--- supercharger-webcontent/build.sh
+++ supercharger-webcontent/build.sh
@@ -4,6 +4,7 @@ PROJECT_VERSION=2.0.0
 BUILD_TIMESTAMP=`date +'%b %d %I:%M %Z'`
 DIR_PROJECT='project'
 DIR_BUILT='project-built'
+FILE_CHANGE_LOG=${DIR_BUILT}/changelog.txt
 
 echo "------------------------------------------------"
 echo "PROJECT_VERSION: ${PROJECT_VERSION}"
@@ -36,5 +37,9 @@ sed -i "s/\${project.version}/${PROJECT_VERSION}/g" ${DIR_BUILT}/version.json
 #
 # change log
 #
-# TODO:
-# git log --grep='@changeLog' --pretty=format:'%cd || %s' --date=local --since 'Sun Jan 12 15:55:31 2014 -0600'
+TEMP_FILE=/tmp/newlog.txt
+
+git log --grep='@changeLog' --pretty=format:'%cd || %s' --date=local --since 'Sun Jan 12 15:55:31 2014 -0600' | sed 's/ [0-9]*:[0-9]*:[0-9]*//' > ${TEMP_FILE}
+printf "\n" >> ${TEMP_FILE}
+cat ${TEMP_FILE} ${FILE_CHANGE_LOG} > ${FILE_CHANGE_LOG}.tmp
+mv ${FILE_CHANGE_LOG}.tmp ${FILE_CHANGE_LOG}
